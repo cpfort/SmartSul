bc90f552c40e9318b83df5dd54b1e6b8802f2020
cpfort <caiocesarportela1992@gmail.com>
2025-04-25 17:24:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const cors = require('cors');
 const { scheduleMessage } = require('./scheduler');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 
 app.use(cors());
 app.use(express.json());
